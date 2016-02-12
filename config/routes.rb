ce9a3ec8ea324391d6653d20d5febcd21fd4bb03
ryopark <dlbia1106@gmail.com>
2016-02-12 15:42:56 -0800

    lessonモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   get 'users/show'
+  get 'lessons/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201_602_110_332_10) do
+ActiveRecord::Schema.define(version: 20160212222421) do
+
+  create_table 'lessons', force: :cascade do |t|
+    t.string   'name',       default: '',   null: false
+    t.string   'prof_name',  default: '',   null: false
+    t.boolean  'in_class',   default: true
+    t.string   'book_name'
+    t.datetime 'created_at',                null: false
+    t.datetime 'updated_at',                null: false
+  end
 
   create_table 'users', force: :cascade do |t|
-    t.string   'email', default: '', null: false
-    t.string   'encrypted_password', default: '', null: false
+    t.string   'email',                  default: '',          null: false
+    t.string   'encrypted_password',     default: '',          null: false
     t.string   'reset_password_token'
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
+    t.integer  'sign_in_count',          default: 0,           null: false
     t.datetime 'current_sign_in_at'
     t.datetime 'last_sign_in_at'
     t.string   'current_sign_in_ip'
@@ -28,14 +37,14 @@ ActiveRecord::Schema.define(version: 201_602_110_332_10) do
     t.datetime 'confirmed_at'
     t.datetime 'confirmation_sent_at'
     t.string   'unconfirmed_email'
-    t.integer  'failed_attempts', default: 0, null: false
+    t.integer  'failed_attempts',        default: 0,           null: false
     t.string   'unlock_token'
     t.datetime 'locked_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+    t.datetime 'created_at',                                   null: false
+    t.datetime 'updated_at',                                   null: false
     t.string   'provider'
     t.string   'uid'
-    t.string   'username', default: 'anonymous'
+    t.string   'username',               default: 'anonymous'
   end
 
   add_index 'users', ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
