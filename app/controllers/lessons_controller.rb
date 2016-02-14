e34e4e287b93bb774561f80d422377ffc4edf3ee
ryopark <dlbia1106@gmail.com>
2016-02-13 16:45:56 -0800

    showページ追加

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,8 @@ class LessonsController < ApplicationController
   def index
     @lessons = Lesson.all
   end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'users/show'
   get 'lessons/index'
+  get 'lessons/:id', to: 'lessons#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
