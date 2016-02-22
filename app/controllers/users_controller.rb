8b8957e48c66e26a4d40f534a221bc24a88d2a2c
ryopark <dlbia1106@gmail.com>
2016-02-22 14:25:02 -0800

    ログイン後のページ、validate等

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  before_action :sign_in_required, only: [:show]
   def index
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    end
   end
 
   def show
