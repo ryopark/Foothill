2eb1cfd807977cdcf5ab57b33659e4f63fffe3d6
s-takuya <takuyashiratake0622@icloud.com>
2016-03-20 15:02:33 +0900

    Change root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-  root to: 'top#index'
+  root to: 'sell_books#index'
 
   resources :top, only: [:index]
 
