fd7e6b83d2eb0c371b9731166ca5a543488798f0
s-takuya <takuyashiratake0622@icloud.com>
2016-04-19 14:55:28 +0900

    Add scope

--- app/models/message_group.rb
+++ app/models/message_group.rb
@@ -5,6 +5,8 @@ class MessageGroup < ActiveRecord::Base
   validates :seller_id, presence: true
   validates :buyer_id, presence: true
 
+  scope :request, -> current_user { where(buyer_id: current_user.id) }
+
   def create(message_group, buyer, seller)
     message_group.buyer_id = buyer.id
     message_group.seller_id = seller.id
