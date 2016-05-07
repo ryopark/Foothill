b9acbe4f684142888655179403447c2c1e3c1793
ryopark <dlbia1106@gmail.com>
2016-05-06 21:05:11 -0700

    css asets compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :scss
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
