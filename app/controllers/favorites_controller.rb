ee9f4ee25407ded6337a09abadbd92d7cc1310cf
s-takuya <takuyashiratake0622@icloud.com>
2016-05-25 16:06:18 +0900

    User favorites list

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,6 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @books = current_user.votes.up.for_type(SellBook).order('created_at desc').votables
-  end
-
   def create
     @book = SellBook.find(params[:sell_book_id])
     @book.liked_by current_user
